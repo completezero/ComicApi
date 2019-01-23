0996ba3a8082a1d21c1d7f32ac144e41a9e8c9e2
BL-HORACEK\michal.horacek <michal.horacek@blogic.cz>
2019-01-23 11:52:29 +0100

    osetreni internetu

--- app/src/main/java/com/example/michalhoracek/comicapp/MainActivity.java
+++ app/src/main/java/com/example/michalhoracek/comicapp/MainActivity.java
@@ -1,10 +1,14 @@
 package com.example.michalhoracek.comicapp;
 
 import android.app.VoiceInteractor;
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -18,6 +22,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
+
 import org.json.JSONObject;
 
 import java.io.IOException;
@@ -31,8 +37,21 @@ public class MainActivity extends AppCompatActivity {
 
     /** Called when the user taps the Send button */
     public void sendMessage(View view) {
-        Intent intent = new Intent(this, DisplayMessageActivity.class);
-        startActivity(intent);
+
+        ConnectivityManager connectivityManager = (ConnectivityManager)getSystemService(Context.CONNECTIVITY_SERVICE);
+        if(connectivityManager.getNetworkInfo(ConnectivityManager.TYPE_MOBILE).getState() == NetworkInfo.State.CONNECTED ||
+                connectivityManager.getNetworkInfo(ConnectivityManager.TYPE_WIFI).getState() == NetworkInfo.State.CONNECTED) {
+            Intent intent = new Intent(this, DisplayMessageActivity.class);
+            startActivity(intent);
+        }
+        else
+        {
+            Toast toast = Toast.makeText(getApplicationContext(), "No internet connection", Toast.LENGTH_SHORT);
+            toast.setGravity(Gravity.CENTER_VERTICAL | Gravity.CENTER_HORIZONTAL, 0, 0);
+            toast.show();
+        }
+
+
     }
 
     /** Called when the user taps the Send button */
